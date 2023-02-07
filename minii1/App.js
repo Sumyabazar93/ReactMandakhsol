bc0c852a6e5486bc860a70d66637b312c6f4cef1
Sumyabazar93 <sumyabazar93@gmail.com>
2023-02-07 13:32:46 +0800

    Lab2 OnPress&OnLongPress

--- minii1/App.js
+++ minii1/App.js
@@ -1,22 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , Image} from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, TextInput, SafeAreaView, Pressable, } from 'react-native';
 
 export default function App() {
+  const callAlert = () => {
+    alert("Button clicked onPress");
+  }
+  const callAlertLong = () => {
+    alert("Button clicked onLongPress");
+  }
   return (
-    <View style={styles.container}>
-      <Text>My first app!</Text>
-      <Text>My first app!</Text>
-      <Image source={"./assets/mycena-leaiana-mushroom-grape-vine-winter-early-inmage-single-orange-southwestern-ontario-166252328.jpeg"} style={{height:200, width:200}}/>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+        <Pressable
+          onPress={() => { callAlert() }}
+          onLongPress={() => { callAlertLong() }}
+        >
+          <Text style={{ color: 'black', fontSize: 30, textAlign: 'center' }} > My first app!</Text>
+          <Image source={require("/Users/sumyabazarbayarkhuu/Desktop/React Mandakh/minii1/back.jpeg")} style={{ height: 1000, width: 1000 }} />
+        </Pressable>
+
+        <Button
+          title="Click me"
+          color="black"
+          onPress={() => { callAlert() }}
+          onLongPress={() => { callAlertLong() }}
+        ></Button>
+
+        <StatusBar style="auto" />
+      </ScrollView>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  ontainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 20,
   },
 });
+
+/*
+1. Nuur tsonh design EXPO deer zurna
+2. Ashgalsan buh compinatuuda onpress(alert) bolon Onlongpress(alert long press) deer alert duudag bolno
+3. 
+*/
